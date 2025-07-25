98f4179ddd9100377823fa834e10992ea3a08f2f
zYuna646 <anggarangga646@gmail.com>
2025-07-25 10:21:07 +0800

    Update app configuration to support new project name 'Makuta'; modify build settings for Android, including gradle commands for different environments, and update dependencies in package files for improved stability and performance.

--- android/app/src/main/java/com/zychan/makuta/MainActivity.kt
+++ android/app/src/main/java/com/zychan/makuta/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.zychan.absencemobile
+package com.zychan.makuta
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/zychan/makuta/MainApplication.kt
+++ android/app/src/main/java/com/zychan/makuta/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.zychan.absencemobile
+package com.zychan.makuta
 
 import android.app.Application
 import android.content.res.Configuration
