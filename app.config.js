d60f919bb86e2c3784348c7eb3b651c1f4c5b4df
zYuna646 <anggarangga646@gmail.com>
2026-03-02 00:37:37 +0800

    refactor: simplify app.config.js by removing unnecessary wrapper
    The previous configuration used a function wrapper and merged Expo Metro config unnecessarily. This simplifies the structure to directly export the Expo configuration object, making the file cleaner and easier to understand.

--- app.config.js
+++ app.config.js
@@ -1,37 +1,31 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const appJson = require('./app.json'); // ambil config dari app.json
+const appJson = require('./app.json');
 
-module.exports = ({ config }) => {
-  return {
-    ...config, // merge default dari app.json
-    ...getDefaultConfig(__dirname),
-    expo: {
-      ...appJson.expo, // pakai semua config dari app.json
-      extra: {
-        ...appJson.expo.extra,
-        eas: {
-          projectId: "ce6f28a5-b3ab-4ebb-9504-e2d385c2434a", // kalau mau override
-        },
+module.exports = {
+  expo: {
+    ...appJson.expo,
+    extra: {
+      ...appJson.expo.extra,
+      eas: {
+        projectId: "ce6f28a5-b3ab-4ebb-9504-e2d385c2434a",
       },
-      plugins: [
-        ...(appJson.expo.plugins || []),
-        [
-          "expo-build-properties",
-          {
-            android: {
-              compileSdkVersion: 35,
-              targetSdkVersion: 35,
-              buildToolsVersion: "35.0.0",
-            },
-            ios: {
-              useFrameworks: "static",
-              useModularHeaders: true,
-              buildReactNativeFromSource: true,
-            },
+    },
+    plugins: [
+      ...(appJson.expo.plugins || []),
+      [
+        "expo-build-properties",
+        {
+          android: {
+            compileSdkVersion: 35,
+            targetSdkVersion: 35,
+            buildToolsVersion: "35.0.0",
           },
-        ],
+          ios: {
+            useFrameworks: "static",
+            useModularHeaders: true,
+            buildReactNativeFromSource: true,
+          },
+        },
       ],
-    },
-    },
-  };
+    ],
+  },
 };
